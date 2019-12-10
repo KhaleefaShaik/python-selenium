e9353787cab50363855118fb916304665734e696
khaleefa <shaikkshavali@gmail.com>
2019-12-10 21:26:53 +0530

    Updated keywords in Python

--- PythonConcepts/PythonBasics.py
+++ PythonConcepts/PythonBasics.py
@@ -1,7 +1,24 @@
 '''
 Created on Dec 5, 2019
-
 @author: Khaleefa Shaik
 '''
 
+import keyword
 print("Khaleefa Shaik")
+
+# Keywords in python
+print(keyword.kwlist)
+'''
+'False', 'None', 'True',
+'and', 'as', 'assert',
+'async', 'await', 'break',
+'class', 'continue', 'def',
+'del', 'elif', 'else',
+'except', 'finally', 'for',
+'from', 'global', 'if',
+'import', 'in', 'is',
+'lambda', 'nonlocal', 'not',
+'or', 'pass', 'raise',
+'return', 'try', 'while',
+'with', 'yield'
+'''
